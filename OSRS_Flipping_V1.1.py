ace7c942a4e201020aa85d581e3b28b95bf1710b
nicbrowndev <nicholas96brown@gmail.com>
2025-08-15 06:05:01 +1000

    Removed debugging printing from previous push

--- OSRS_Flipping_V1.1.py
+++ OSRS_Flipping_V1.1.py
@@ -108,7 +108,7 @@ if response.status_code == 200:
                     top10.pop(0)
 
 for RsItem in top10:
-    print(RsItem.name,"\n","Buy Price: ",RsItem.price_low,"\n","Sell Price: ",RsItem.price_high,"\n","Low Price Volume (5min): ",RsItem.volume_low_price,"\n","High Price Volume (5min)",RsItem.volume_high_price,"\n","Daily Profit: ",RsItem.daily_profit,"\n","ROI: ",RsItem.roi, "%","\n","total sell value ")
+    print(RsItem.name,"\n","Buy Price: ",RsItem.price_low,"\n","Sell Price: ",RsItem.price_high,"\n","Low Price Volume (5min): ",RsItem.volume_low_price,"\n","High Price Volume (5min)",RsItem.volume_high_price,"\n","Daily Profit: ",RsItem.daily_profit,"\n","ROI: ",RsItem.roi, "%","\n")
 
 if input("Press Enter to exit"):
     exit()
